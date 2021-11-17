39c265a6b0fa917ff6dd39cf83a89b08f348ff4e
lenovo <anilannangi25@gmail.com>
2021-11-17 11:08:48 +0530

    Added to h1 tag with hello world

--- sk-vs2019-GitDemo55/Startup.cs
+++ sk-vs2019-GitDemo55/Startup.cs
@@ -32,7 +32,7 @@ namespace sk_vs2019_GitDemo55
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("<h1>Hello World!</h1>");
                 });
             });
         }
